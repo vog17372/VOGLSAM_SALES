df822d021934c5566f47eabc961b53a23f65af8c
fe_vo <Felix@me.com>
2017-04-23 15:51:23 +0200

    Customer und Customers hinzugefügt

---
+++ DBITEST/src/domain/Customer.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class Customer
+{
+
+	public Customer()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ DBITEST/src/domain/Customers.java
@@ -0,0 +1,13 @@
+package domain;
+
+import java.util.ArrayList;
+
+public class Customers
+{
+	private ArrayList<Customer> customers = new ArrayList<>();
+	public Customers()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
